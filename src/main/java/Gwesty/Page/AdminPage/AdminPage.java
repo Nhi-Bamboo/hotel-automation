93dd38d29b490a6087149a37f0ccbb5f3cc79aec
PC <nguyentranhoaithuong10032003@gmail.com>
2024-12-09 19:21:48 +0700

    code mới thêm

--- src/main/java/Gwesty/Page/AdminPage/AdminPage.java
+++ src/main/java/Gwesty/Page/AdminPage/AdminPage.java
@@ -2,10 +2,14 @@ package Gwesty.Page.AdminPage;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class AdminPage {
     WebDriver driver;
-
+    By submenu = By.xpath("//li[@class='nav-item open']");
     By viewAllRoomTypeNavLinkLocator = By.xpath("//span[@class='title'][text()='View All Room Types']");
     public AdminPage(WebDriver driver) {
         this.driver = driver;
@@ -13,6 +17,8 @@ public class AdminPage {
     public void clickMenu(String option) {
         String xpathMenu = String.format("//span[@class='title'][text()='%s']",option);
         driver.findElement(By.xpath(xpathMenu)).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated((submenu)));
     }
     public void clickSubMenu(String option) {
         String xpathSubMenu = String.format("//span[@class='title'][text()='%s']",option);
